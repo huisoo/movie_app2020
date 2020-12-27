67796d13c1db2db4156799f16c26fe4483900f85
hui921 <huiwater@naver.com>
2020-12-27 20:52:28 +0900

    final code

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About(props){
+    return(
+        <div className="about_container">
+            <span>
+                "Freedom is the freedom to say that two plus two make four."
+            </span>
+            <span>--George ORwell</span>
+        </div>
+    );
+}
+
+export default About;
